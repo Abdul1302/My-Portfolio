bee12901528edb617d3722b24ebf7b00de4ff6c6
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-09-03 22:35:22 +0500

    commt

--- portfolio/src/app/resume/page.tsx
+++ portfolio/src/app/resume/page.tsx
@@ -1,4 +1,61 @@
-import React from 'react'
+"use client";
+
+import { Value } from "@radix-ui/react-select";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaPython,
+  FaReact,
+  FaNodeJs} from "react-icons/fa"
+
+import{
+  SiPandas,
+  SiScikitlearn,
+  SiTypescript,
+  SiTensorflow,
+  SiTailwindcss,
+  SiNextdotjs
+}  from "react-icons/si"
+
+// about data
+const about={
+  title:"About me",
+  description:'',
+  info:[
+    {
+      fieldName:"Name",
+      fieldValue: "Abdul Rehman"
+    },
+    {
+      fieldName:"Phone",
+      fieldValue: "(+92) 3228298414"
+    },
+    {
+      fieldName:"Experience",
+      fieldValue: "1+ Year"
+    },
+    {
+      fieldName:"Nationality",
+      fieldValue: "Pakistan"
+    },
+    {
+      fieldName:"Email",
+      fieldValue: "abdul.rehman132802@gmail.com"
+    },
+    {
+      fieldName:"Languages",
+      fieldValue: "English, Urdu"
+    }
+    
+  ]
+
+}
+
+// experience data
+const experience={
+  
+}
 
 const Resume = () => {
   return (

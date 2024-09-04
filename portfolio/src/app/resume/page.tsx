91c63b58928f2c7ae6b1bf8a148245c145569dcf
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-09-04 12:55:34 +0500

    commit

--- portfolio/src/app/resume/page.tsx
+++ portfolio/src/app/resume/page.tsx
@@ -7,59 +7,224 @@ import {
   FaJs,
   FaPython,
   FaReact,
-  FaNodeJs} from "react-icons/fa"
+  FaNodeJs
+} from "react-icons/fa"
 
-import{
+import {
   SiPandas,
   SiScikitlearn,
   SiTypescript,
   SiTensorflow,
   SiTailwindcss,
-  SiNextdotjs
-}  from "react-icons/si"
+  SiNextdotjs,
+  SiNumpy,
+  SiStreamlit,
+  SiLangchain
+} from "react-icons/si"
 
 // about data
-const about={
-  title:"About me",
-  description:'',
-  info:[
+const about = {
+  title: "About me",
+  description: '',
+  info: [
     {
-      fieldName:"Name",
+      fieldName: "Name",
       fieldValue: "Abdul Rehman"
     },
     {
-      fieldName:"Phone",
+      fieldName: "Phone",
       fieldValue: "(+92) 3228298414"
     },
     {
-      fieldName:"Experience",
+      fieldName: "Experience",
       fieldValue: "1+ Year"
     },
     {
-      fieldName:"Nationality",
+      fieldName: "Nationality",
       fieldValue: "Pakistan"
     },
     {
-      fieldName:"Email",
+      fieldName: "Email",
       fieldValue: "abdul.rehman132802@gmail.com"
     },
     {
-      fieldName:"Languages",
+      fieldName: "Languages",
       fieldValue: "English, Urdu"
     }
-    
+
   ]
 
 }
 
 // experience data
-const experience={
-  
+const experience = {
+  icon: '\assets\resume-2\badge.svg',
+  title: 'My experience',
+  description: '',
+  items: [
+    {
+      company: 'Itsolera Pvt Ltd.',
+      position: 'NLP Engineer',
+      duration: 'Jun(2024) - Present'
+    },
+    {
+      company: 'Bawdicsoft Pvt Ltd.',
+      position: 'Junior Ai Engineer',
+      duration: 'August(2023) - May(2024)'
+    },
+  ]
 }
+
+//education data
+const education = {
+  icon: '\assets\resume-2\cap.svg',
+  title: 'My education',
+  description: '',
+  items: [
+    {
+      institution: 'Federal Urdu University Of Arts, Science & Technology',
+      position: 'Bachelors Of Science In Computer Science',
+      duration: '2020 - 2024'
+    },
+    {
+      institution: 'Saylani Mass IT',
+      position: 'Generative-Ai & Chatbot Development ',
+      duration: 'Present'
+    },
+    {
+      institution: 'Saylani Mass IT',
+      position: 'Ai & Ml Course',
+      duration: '2021 - 2022'
+    },
+    {
+      institution: 'Government Superior Science College',
+      position: 'Intermediate In Pre Engineering',
+      duration: '2018 - 2020'
+    },
+    {
+      institution: 'Al Mumtaz Academy',
+      position: 'Matriculation In Computer Science',
+      duration: '2016 - 2018'
+    },
+    {
+      institution: 'Al Mumtaz Academy',
+      position: 'Hifz ul Quran',
+      duration: '2013 - 2016'
+    },
+
+  ]
+}
+
+// Skills Data
+const skills = {
+  title: 'My skills',
+  description: '',
+  skillList: [
+    {
+      icon: <FaPython />,
+      name: 'python',
+    },
+    {
+      icon: <SiNumpy />,
+      name: 'numpy',
+    },
+    {
+      icon: <SiPandas />,
+      name: 'pandas',
+    },
+    {
+      icon: <SiScikitlearn />,
+      name: 'scikit learn',
+    },
+    {
+      icon: <SiTensorflow />,
+      name: 'tensorflow',
+    },
+    {
+      icon: <SiStreamlit />,
+      name: 'streamlit',
+    },
+    {
+      icon: <SiLangchain />,
+      name: 'langchain',
+    },
+    {
+      icon: <FaHtml5 />,
+      name: 'html 5',
+    },
+    {
+      icon: <FaCss3 />,
+      name: 'css 3',
+    },
+    {
+      icon: <FaJs />,
+      name: 'javascript',
+    },
+    {
+      icon: <SiTypescript />,
+      name: 'typescript',
+    },
+    {
+      icon: <FaReact />,
+      name: 'react.js',
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: 'next.js',
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: 'tailwind.css',
+    },
+    {
+      icon: <FaNodeJs />,
+      name: 'node.js',
+    },
+
+  ]
+}
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
+} from '@/components/ui/tabs'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@radix-ui/react-tooltip";
+
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { motion } from 'framer-motion'
 
 const Resume = () => {
   return (
-    <div>Resume Page</div>
+    <motion.div initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1, transition: { deley: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList>
+            <TabsTrigger>Experience</TabsTrigger>
+            <TabsTrigger>Education</TabsTrigger>
+            <TabsTrigger>Skills</TabsTrigger>
+            <TabsTrigger>About me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div>
+            content 
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
   )
 }
 

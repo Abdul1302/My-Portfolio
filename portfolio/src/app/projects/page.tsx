c92101171c656bad1c660129fc79cd45000e5652
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-09-15 20:22:04 +0500

    commit

--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -2,7 +2,6 @@
 
 import { FiGithub } from "react-icons/fi"
 import Link from "next/link";
-import { text } from "stream/consumers";
 import { motion } from 'framer-motion'
 
 const services = [
@@ -10,25 +9,31 @@ const services = [
     num: '01',
     title: 'Story Creation & Completion Model',
     description: 'Developed a model to transform descriptions into coherent stories using RAG and convert these stories into audio narrations. Performed prompt engineering using LLaMA-3 and integrated a pyttsx3 aibrary for text-to-speech task for high-quality audio output. This model can also be able to complete the un-completed stories.',
-    href: ''
+    href: 'https://github.com/Abdul1302/Project11-Story-Generation-Completion-Chatbot'
   },
   {
     num: '02',
-    title: 'AI Healthcare Chatbot System',
+    title: 'Ai Healthcare Chatbot System',
     description: 'This chatbot System is designed to provide healthcare support online, Guiding patients towards appropriate treatment options. Performed RAG and prompt engineering using LLaMA-3 as healthcare assistant, integrated a pyttsx laibrary model for audio output. Used map-api for finding the nearest hospital or clinic for the diseases.',
-    href: ''
+    href: 'https://github.com/Abdul1302/Project12-AI-Healthcare-Chatbot'
   },
   {
     num: '03',
     title: 'Company-Based Chatbot',
     description: "Developed a company-specific chatbot utilizing the LLaMA-3 versatile model with the Groq API and RAG. The model is trained on the company's proprietary data, allowing it to deliver accurate, context-driven responses tailored to the organization's needs, enhancing internal communication and customer support capabilities.",
-    href: ''
+    href: 'https://github.com/Abdul1302/Project14-Company-Chatbot'
   },
   {
     num: '04',
-    title: 'AI Speech-to-Speech Model',
+    title: 'Ai Speech-to-Speech Model',
     description: 'Developed a speech-to-speech chatbot using the LLaMA-3 versatile model for conversational responses. Integrated Whisper Large-V3 for speech-to-text conversion and the pyttsx3 library for seamless text-to-speech output. This model enhance the user experience in voice-based communication systems.',
-    href: ''
+    href: 'https://github.com/Abdul1302/Project15-Ai-Speech-to-Speech-Model'
+  },
+  {
+    num: '05',
+    title: 'Truth Seeker Ai Model',
+    description: 'Developed a research-based AI forensics model focusing on image analysis. Conducted research to create the model. Utilized Deep Learning algorithm (CNN), Classifications Techniques & Error Level Analysis (ELA). Successfully built the model with an accuracy rate of 90%.',
+    href: 'https://github.com/Abdul1302/Project-09-Authentify-Ai'
   },
 
 ]
@@ -44,32 +49,27 @@ const Services = () => {
         >
           {services.map((service, index) => {
             return (
-              <div key={index} className="flex-1 flex flex-col justify-center gap-4 group">
+                <div key={index} className="flex-1 flex flex-col justify-center gap-4 group">
 
-                {/* top */}
-                <div className="w-full flex justify-between items-center ">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                    {service.num}
+                  {/* top */}
+                  <div className="w-full flex justify-between items-center ">
+                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                      {service.num}
+                    </div>
+                    <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:scale-110 ">
+                      <FiGithub className="text-primary text-3xl" />
+                    </Link>
                   </div>
-                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:scale-110 ">
-                    <FiGithub  className="text-primary text-3xl"/>
-                  </Link>
+
+                  {/* title */}
+                  <h2 className="text-[24px] font-bold loading-none text-white group-hover:text-accent transition-all duration-500  ">{service.title}</h2>
+
+                  {/* Description */}
+                  <p className="text-white/60">{service.description}</p>
+
+                  {/* border */}
+                  <div className="border-b border-white/20 w-full"></div>
                 </div>
-
-                {/* title */}
-                <h2 className="text-[24px] font-bold loading-none text-white group-hover:text-accent transition-all duration-500  ">{service.title}</h2>
-
-                {/* Description */}
-                <p className="text-white/60">{service.description}</p>
-
-                {/* border */}
-                <div className="border-b border-white/20 w-full"></div>
-
-                {/* Note */}
-                <div>
-                  
-                </div>
-              </div>
             )
           })}
         </motion.div>

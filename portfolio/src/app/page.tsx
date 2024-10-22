4fb2d370dacc0c4773920aba050395e5e7208d1a
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-10-22 10:53:33 +0500

    commit

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { FiDownload } from 'react-icons/fi'
+import Link from "next/link"
 
 //component
 import Social from "@/components/Social"
@@ -19,30 +20,32 @@ const Home = () => {
               Hello I'm <br /> <span className="text-accent">Abdul Rehman</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 ">
-            I am an experienced AI and ML Engineer with a strong focus on Natural Language Processing (NLP) 
-            & Generative AI. My expertise lies in developing intelligent systems that understand and generate human language.
+              I am an experienced AI and ML Engineer with a strong focus on Natural Language Processing (NLP)
+              & Generative AI. My expertise lies in developing intelligent systems that understand and generate human language.
             </p>
 
             {/* button & socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="" download="">
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-10" 
-                iconStyles="w-14 h-14 text-xl border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                <Social containerStyles="flex gap-10"
+                  iconStyles="w-14 h-14 text-xl border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
 
           {/* Photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0 ">
-            <Photo/>
+            <Photo />
           </div>
         </div>
       </div>
-      <Stats/>
+      <Stats />
 
     </section>
   )
